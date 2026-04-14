6e643b9bf2703ccf623bb464ecd58ad621bf9445
malathi murugan <annannmurugan262@gmail.com>
2026-04-14 12:28:08 +0530

    fix case-sensitive image import

--- frontend/src/pages/floatingCallButton.jsx
+++ frontend/src/pages/floatingCallButton.jsx
@@ -35,7 +35,15 @@ const FloatingCallButton = () => {
       }}
       aria-label="Call us"
     >
-      📞
+      <svg
+  xmlns="http://www.w3.org/2000/svg"
+  width="24"
+  height="24"
+  fill="white"
+  viewBox="0 0 24 24"
+>
+  <path d="M6.62 10.79a15.91 15.91 0 006.59 6.59l2.2-2.2a1 1 0 011-.24c1.12.37 2.33.57 3.59.57a1 1 0 011 1v3.5a1 1 0 01-1 1A17 17 0 013 6a1 1 0 011-1h3.5a1 1 0 011 1c0 1.26.2 2.47.57 3.59a1 1 0 01-.25 1z"/>
+</svg>
     </a>
   );
 };
